35d8325be400b9de2f00147cb05be49f9d104e56
mohammed-khalaf <khalaf@sparkle.sa>
2020-11-18 18:26:53 -0800

    make model & service & crud functions

---
+++ src/app/models/Employee.ts
@@ -0,0 +1,7 @@
+export class Employee {
+	$key:string;
+	name:string;
+	email:string;
+	mobile:string;
+	details:string;
+}
---
+++ src/app/shared/employee.service.ts
@@ -0,0 +1,59 @@
+import { Injectable } from '@angular/core';
+
+import { AngularFireList , AngularFireObject , AngularFireDatabase } from '@angular/fire/database';
+
+import { Employee } from '../models/Employee';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+
+  employeeListRef : AngularFireList<any>;
+  employeeRef : AngularFireObject<any>;
+
+  constructor(private db:AngularFireDatabase ) 
+  {
+  	this.employeeListRef = db.list('/employee')
+  }
+
+  createEmployee(emp:Employee)
+  {
+  	return this.employeeListRef.push({
+  		name:emp.name,
+  		email:emp.email,
+		mobile:emp.mobile,
+		details:emp.details,
+  	})
+
+  }
+
+  	getEmployee(id:string)
+  	{
+  		return this.employeeRef = this.db.object('/employee/'+id)
+  	}
+
+  	getEmployeeList()
+  	{
+  		return this.employeeListRef = this.db.list('/employee')
+  	}
+
+
+  	UpdateEmployee(id,emp:Employee)
+	{
+	  	return this.employeeRef.update({
+	  		name:emp.name,
+	  		email:emp.email,
+			mobile:emp.mobile,
+			details:emp.details,
+	  	})
+	}
+
+	deleteEmployee(id:string)
+  	{
+  		this.employeeRef = this.db.object('/employee/'+id)
+  		this.employeeRef.remove()
+  	}
+
+  
+}
